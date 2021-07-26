43435eb0cac386d64b9f0fa27d0e48a891b5a2cd
Reid <rlj0713@gmail.com>
2021-07-25 20:33:45 -0400

    commit tree 9d9c246bb4f87ccbb404bf79629ad477ed312096

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+let dodger = document.getElementById("dodger"); 
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 3}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 3}px`;
+    }
+}
+
+function moveDodgerUp() {
+    let bottomNumbers = dodger.style.bottom.replace("px", "");
+    let bottom = parseInt(bottomNumbers, 10);
+  
+    dodger.style.bottom = `${bottom + 3}px`;
+}
+
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    } else if (e.key === "ArrowUp") {
+        moveDodgerUp()
+    } else {
+        console.log("Not a programmed key")
+    }
+  });
